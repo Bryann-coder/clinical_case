34ad23292cf4ae1c01fea99b6475127377398142
Bryann <yobobryan@gmail.com>
2025-11-07 16:07:20 +0100

    paramètre de cors

--- cases_api/settings.py
+++ cases_api/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',  
     # Dépendances
     'rest_framework',
     'rest_framework.authtoken',
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'cases_api.urls'
